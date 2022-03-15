661d5bd5b524f418bfd7ca15c8bed02cb5149d1d
Uchida310 <mitsuya.uchida310@gmail.com>
2022-03-15 23:42:30 +0900

    create history vuetify 8/10

--- home-port-skill/src/views/MyHistory.vue
+++ home-port-skill/src/views/MyHistory.vue
@@ -1,17 +1,234 @@
 
 <template>
   <div>
-    <h1>人生経歴</h1>
-    <p>カジュアルな職務経歴をイメージ</p>
+    <v-app>
+      <v-container fill-height class="main-content">
+        <h1>～簡単な経歴紹介～</h1>
+        <v-card
+          class="v-content-style"
+          tile
+          elevation="12"
+          light
+          style="margin-top:20px;"
+        >
+          <v-card-title
+            >高校卒業後、リサイクルショップでアルバイトを始める。</v-card-title
+          >
+          <v-card-subtitle>2014</v-card-subtitle>
+           <v-divider></v-divider>
+          <v-card-text>
+            <div style="margin-top:10px;">
+              特に将来の目標もなく、卒業と同時に一人暮らしを開始。21まで適当に生きる。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+          class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>人生の転機</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              従弟が16歳で他界し、自分の人生について考えるようになる<br />
+              適当に生きることの無意味さを感じ、アルバイトを真面目にするようになる。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+          class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>家電担当へ</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              家電担当を本格的に任される。<br />
+              買取価格や販売価格を自分で管理し、目標を達成できるよう考えて働くようになる。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+         class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>県内売上1位へ</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              売上目標を達成し続けるのと同時に、県内家電売り上げもトップになり、自信を持つようになる。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+         class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>売上の限界</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              アルバイトの立場ではこれ以上売り上げを伸ばせないと思い、正社員として働くか真剣に考える。<br/>
+              自分の人生を見つめ直し、学生の頃の夢であったエンジニアとして働きたいと思い始める。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+         class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>エンジニアを目指し、東京へ</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              とりあえず働いてみようと思い、東京のSES系の会社へ就職し上京。
+              同時にUnity開発の手伝いを行う。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+          class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>出向先を変更させて貰えず、退職</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              出向先の変更を契約更新時に断られ、退職。
+              Unity開発の手伝いも収入の関係で諦める。
+              家族と話し合い、一度地元へ帰省することに。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+          class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>体調回復をしながらアルバイトを行う</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              地元で精神的な回復をしながら、リサイクルショップのアルバイトをする。
+              体調が回復したのち、再びエンジニアを目指す。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+          class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>職業訓練校へ</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              ハローワークで職業訓練校を紹介され、興味を持つ。
+              無事合格し、職業訓練校に通う。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+          class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>webエンジニアを目指す</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              講師や外部講話で刺激を受け、技術的な理由で諦めたweb系エンジニアを目指し学習を始める。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+          class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>JavaScript学習</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              講師の方と相談して、JavaScriptの学習を独学で始める。<br />
+              躓きながらも、ポートフォリオの作成やソースコードのトレースが出来るようになる。
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+          class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>フレームワークでSPAサイト作成</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              webエンジニアを目指そうと決めた際に印象に残っていたSPAサイトの学習を始める。<br />
+              3週間の限られた時間で、無事学習とサイト製作を終えてネット上に公開する
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card
+         class="v-content-style"
+          tile
+          elevation="12"
+          light
+        >
+          <v-card-title>就職活動～現在～</v-card-title>
+          <v-card-subtitle>2014</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text>
+            <div>
+              今まで製作したポートフォリオと資格を手に、就職活動を行う。<br />
+              自分の成長と人への貢献を両立できる会社への就職を目指す。
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-app>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.v-content-style {
+  margin-bottom: 40px;
+  width: 600px;
+}
 
+.main-content {
+  flex-direction: column;
+}
 </style>
